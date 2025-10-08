1de33608931109bb51cb51006495176f5e05b63a
slemeshaev <lemeshaevs@gmail.com>
2025-10-08 20:46:36 +0300

    Implementation of the server creation in the project

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	todo "github.com/slemeshaev/todo/internal"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error occured while running http-server: %s", err.Error())
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/slemeshaev/todo
 
 go 1.24.5
+
+require (
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/net v0.45.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
