daddf25cf10d0ca04b81f610614e656076859682
slemeshaev <lemeshaevs@gmail.com>
2025-10-09 23:44:52 +0300

    Add handlers and endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 
+	"github.com/slemeshaev/todo/internal/handler"
 	todo "github.com/slemeshaev/todo/internal/service"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRouters()); err != nil {
 		log.Fatalf("error occured while running http-server: %s", err.Error())
 	}
 }
