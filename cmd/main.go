57771d2f751f1eba62921a28c568329d44b4659a
slemeshaev <lemeshaevs@gmail.com>
2025-10-13 21:30:07 +0300

    Add Viper configuration management
    - Add github.com/spf13/viper package
    - Add config.yml configuration file
    - Implement initConfig() function in main
    - Update server to use viper for port configuration

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/slemeshaev/todo/internal/repository"
 	todo "github.com/slemeshaev/todo/internal/server"
 	"github.com/slemeshaev/todo/internal/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRouters()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRouters()); err != nil {
 		log.Fatalf("error occured while running http-server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
