38bdfb5f1e53d7531be1dc04acd7fd9023ccb90b
slemeshaev <lemeshaevs@gmail.com>
2025-11-02 23:44:55 +0300

    Implement JWT authentication with database integration
    - Add signIn handler with input validation
    - Implement GetUser method in AuthPostgres repository
    - Add JWT token generation with user claims
    - Update Authorization interface with new methods
    - Add database tag for id field in user struct
    - Handle password hashing in token generation

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -3,15 +3,26 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	todo "github.com/slemeshaev/todo/internal/models"
 	"github.com/slemeshaev/todo/internal/repository"
 )
 
-const salt = "jdkafjajl84uo8afsf44fafadfg"
+const (
+	salt       = "jdkafjajl84uo8afsf44fafadfg"
+	signingKey = "fajdklfjl48fua9ha9fhafh"
+	tokenTTL   = 12 * time.Hour
+)
 
 type AuthService struct {
 	repo repository.Authorization
+}
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
 }
 
 func NewAuthService(repo repository.Authorization) *AuthService {
@@ -21,6 +32,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(12 * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
