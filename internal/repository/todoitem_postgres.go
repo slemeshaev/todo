06df8850614545d4ca2fb5d8fb9bd2114e6aeda7
slemeshaev <lemeshaevs@gmail.com>
2025-11-17 22:35:01 +0300

    Implement todo item creation functionality
    - Add Create method to handler with JSON binding and error handling
    - Update service layer to validate list ownership before creation
    - Implement repository with transaction for atomic database operations
    - Modify interfaces to include user ID parameter for authorization

---
+++ internal/repository/todoitem_postgres.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	todo "github.com/slemeshaev/todo/internal/models"
+)
+
+type TodoItemPostgres struct {
+	db *sqlx.DB
+}
+
+func NewTodoItemPostgres(db *sqlx.DB) *TodoItemPostgres {
+	return &TodoItemPostgres{db: db}
+}
+
+func (r *TodoItemPostgres) Create(listId int, item todo.TodoItem) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	var itemId int
+	createItemQuery := fmt.Sprintf("INSERT INTO %s (title, description) values ($1, $2) RETURNING id", todoItemsTable)
+
+	row := tx.QueryRow(createItemQuery, item.Title, item.Description)
+	err = row.Scan(&itemId)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	createListItemsQuery := fmt.Sprintf("INSERT INTO %s (list_id, item_id) values ($1, $2)", listItemsTable)
+	_, err = tx.Exec(createListItemsQuery, listId, itemId)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	return itemId, tx.Commit()
+}
---
+++ internal/service/todo_item.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	todo "github.com/slemeshaev/todo/internal/models"
+	"github.com/slemeshaev/todo/internal/repository"
+)
+
+type TodoItemService struct {
+	repo     repository.TodoItem
+	listRepo repository.TodoList
+}
+
+func NewTodoItemService(repo repository.TodoItem, listRepo repository.TodoList) *TodoItemService {
+	return &TodoItemService{repo: repo, listRepo: listRepo}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, error) {
+	_, err := s.listRepo.GetById(userId, listId)
+	if err != nil {
+		// list does not exists or does not belongs to user
+		return 0, err
+	}
+
+	return s.repo.Create(listId, item)
+}
