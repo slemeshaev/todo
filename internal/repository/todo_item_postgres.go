8bdc1208bf43039b1f5fb189df5d790d3c1e7745
slemeshaev <lemeshaevs@gmail.com>
2025-11-26 22:41:11 +0300

    Add update todo item functionality
    - Implement update endpoint for todo items at PUT /api/items/:id
    - Add Update method to TodoItem service interface and implementation
    - Create UpdateItemInput struct with optional fields and validation
    - Implement SQL update query with dynamic field binding
    - Update handler to use TodoItem service instead of TodoList service
    - Add proper error handling and status responses

--- internal/repository/todo_item_postgres.go
+++ internal/repository/todo_item_postgres.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	todo "github.com/slemeshaev/todo/internal/models"
@@ -70,5 +71,38 @@ func (r *TodoItemPostgres) Delete(userId, itemId int) error {
 		WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $1 AND ti.id = $2`,
 		todoItemsTable, listItemsTable, usersListsTable)
 	_, err := r.db.Exec(query, userId, itemId)
+	return err
+}
+
+func (r *TodoItemPostgres) Update(userId, itemId int, input todo.UpdateItemInput) error {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Title != nil {
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+		args = append(args, *input.Title)
+		argId++
+	}
+
+	if input.Description != nil {
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+		args = append(args, *input.Description)
+		argId++
+	}
+
+	if input.Done != nil {
+		setValues = append(setValues, fmt.Sprintf("done=$%d", argId))
+		args = append(args, *input.Done)
+		argId++
+	}
+
+	setQuery := strings.Join(setValues, ", ")
+	query := fmt.Sprintf(`UPDATE %s ti SET %s FROM %s li, %s ul 
+							WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $%d AND ti.id = $%d`,
+		todoItemsTable, setQuery, listItemsTable, usersListsTable, argId, argId+1)
+	args = append(args, userId, itemId)
+
+	_, err := r.db.Exec(query, args...)
 	return err
 }
