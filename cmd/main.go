9d648f88601b1020f0ff06625f50722792682977
slemeshaev <lemeshaevs@gmail.com>
2025-11-27 23:09:21 +0300

    Implement graceful shutdown for Todo server
    - Add HTTP server with graceful shutdown handling
    - Handle SIGTERM and SIGINT signals
    - Properly close database connections on shutdown
    - Add error logging for server operations

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -42,8 +45,26 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRouters()); err != nil {
-		logrus.Fatalf("error occured while running http-server: %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRouters()); err != nil {
+			logrus.Fatalf("error occured while running http-server: %s", err.Error())
+		}
+	}()
+
+	logrus.Println("Todo Started...")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Todo Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server sutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
 	}
 }
 
